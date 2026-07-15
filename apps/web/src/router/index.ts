d49f4044281c248bed544c8f7e5867f484f89013
LUNA Dev <luna-dev@luna-project.local>
2026-07-15 23:31:38 +0800

    fix: public QR + webHistory router

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   { path: '/gift', name: 'gift', component: () => import('@/pages/GiftPage.vue') },
@@ -12,6 +12,6 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
